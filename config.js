21473c62e4ec72bd086b63edad2f6d6d13a1b713
kinglyl <36796881+kinglyl@users.noreply.github.com>
2020-08-08 20:19:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: '站点状态监控---极客青年',
+  SiteName: '站点状态监控---Oriole-Proxy',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,7 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
   ApiKeys: [
-    'ur894242-90d1eb90acc82624bcaed27a'
+    'ur994937-b1e19db65d460a8700971580'
   ],
 
   // 是否显示监测站点的链接
@@ -26,14 +26,6 @@ window.Config = {
     {
       text: '项目主页',
       url: 'https://github.com/geekyouth/uptime-status'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/geekyouth/'
-    },
-    {
-      text: '博客',
-      url: 'https://java666.cn'
     }
   ]
 };
